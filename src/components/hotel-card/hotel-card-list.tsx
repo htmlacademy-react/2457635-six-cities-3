c6b7153b0814a67aa0a8159bf73ceb4e447583f9
Tatianos7 <152062638+Tatianos7@users.noreply.github.com>
2025-12-05 11:17:53 +0300

    Больше подробностей (часть 1)

--- src/components/hotel-card/hotel-card-list.tsx
+++ src/components/hotel-card/hotel-card-list.tsx
@@ -1,12 +1,12 @@
 import HotelCard from './hotel-card.tsx';
-import { AppProps } from '../../types.ts';
-import { useState } from 'react';
+import {Offers} from '../../types.ts';
 
-type HotelCardListProps = AppProps;
+type HotelCardListProps = {
+  offers: Offers;
+  setCurrentCard: (value: string) => void;
+};
 
-export default function HotelCardList({offers}: HotelCardListProps) {
-
-  const [, setCurrentCard] = useState('');
+export default function HotelCardList({offers, setCurrentCard}: HotelCardListProps) {
 
   return (
     <div className="cities__places-list places__list tabs__content">
