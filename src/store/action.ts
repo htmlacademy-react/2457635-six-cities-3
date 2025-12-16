a19ad31d673ff261d9cebbb399bc6471dd91cebe
Tatianos7 <152062638+Tatianos7@users.noreply.github.com>
2025-12-16 11:35:03 +0300

    Контроль и ограничения (часть 2)

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,12 @@
 import { createAction } from '@reduxjs/toolkit';
-import { City } from '../types/models';
+import { City} from '../types/models';
 
 export const changeTown = createAction('/changeTown', (value: City) => ({
   payload: value,
 }));
 
 export const offers = createAction('/offers');
+
+export const setSorting = createAction('/sorting', (value: string) => ({
+  payload: value
+}));
